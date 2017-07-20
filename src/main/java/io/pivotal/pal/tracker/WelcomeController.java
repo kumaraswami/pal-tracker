f5747ab45c385c0b27949f9acfcdfbedb3feca30
Tyson Gern <tgern@pivotal.io>
2017-07-20 13:56:50 -0600

    commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,9 +2,16 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
+
+	private String welcomeMsg;
+
+	public WelcomeController(@Value("${WELCOME_MESSAGE}") String welcomeMsg) {
+		welcomeMsg = this.welcomeMsg;
+	}
 
 	    @GetMapping("/")
 	        public String sayHello() {
